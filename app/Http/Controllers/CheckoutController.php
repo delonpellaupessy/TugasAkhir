9f89bf32b1be457bb66abf49525216a4f1f0f2aa
benben4567 <benben4567@gmail.com>
2020-10-04 00:03:57 +0700

    Add checkout callback

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -92,5 +92,54 @@ class CheckoutController extends Controller
     public function callback (Request $request)
     {
 
+        dd($request);
+        $transaction = $request->transaction_status;
+        $type = $request->payment_type;
+        $order_id = $request->order_id;
+        $fraud = $request->fraud_status;
+
+        if ($transaction == 'capture') {
+            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+            if ($type == 'credit_card'){
+                if($fraud == 'challenge'){
+                // TODO set payment status in merchant's database to 'Challenge by FDS'
+                // TODO merchant should decide whether this transaction is authorized or not in MAP
+                echo "Transaction order_id: " . $order_id ." is challenged by FDS";
+                }
+                else {
+                // TODO set payment status in merchant's database to 'Success'
+                echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
+                }
+            }
+        } else if ($transaction == 'settlement') {
+            // TODO set payment status in merchant's database to 'Settlement'
+            echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
+            }
+            else if($transaction == 'pending'){
+            // TODO set payment status in merchant's database to 'Pending'
+            echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+            }
+            else if ($transaction == 'deny') {
+            // TODO set payment status in merchant's database to 'Denied'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+            }
+            else if ($transaction == 'expire') {
+            // TODO set payment status in merchant's database to 'expire'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
+            }
+            else if ($transaction == 'cancel') {
+            // TODO set payment status in merchant's database to 'Denied'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+        }
+    }
+
+    public function check()
+    {
+        // update payment status
+        $status = 'PAID';
+        $transaction = Transaction::whereCode('STORE-8667')->update([
+            "transaction_status" => $status
+        ]);
+        dd($transaction);
     }
 }
